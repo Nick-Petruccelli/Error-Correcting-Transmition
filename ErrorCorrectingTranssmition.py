fd02066278d1342d93524393a6c5ea3eae095227
Nick-Petruccelli <NickPetro18@gmail.com>
2023-10-08 12:08:30 -0400

    added method to collapse redundent data by vote

--- ErrorCorrectingTranssmition.py
+++ ErrorCorrectingTranssmition.py
@@ -29,22 +29,46 @@ def get_png_from_binary(binary_data: BitArray, file_name: str)->None:
     image.save(file_name+".png")
 
 
-def make_binary_redundent(binary_data: BitArray, times: int)->BitArray:
+def make_binary_redundent(binary_data: BitArray, copys: int)->BitArray:
     """
     takes binary data and repeats it n times
     :param binary_data: data to be made redundent
-    :param times: number of times you want data to be repeated. ex) if times = 2, data will be twice as long as before
+    :param copys: how manny copys of the data you want
     """
-    redundent_data = binary_data
-    for i in range(times-1):
+    redundent_data = BitArray(length=0)
+    for i in range(copys):
         redundent_data.append(binary_data)
     return redundent_data
 
+def vote_corect_data(redundent_data: BitArray, copys: int)->BitArray:
+    original_data_len = (int)(len(redundent_data)/(copys))
+    collapsed_data = BitArray(length=original_data_len)
+    for i in range(original_data_len-1):
+        num_ones = 0
+        num_zeros = 0
+        for j in range(copys-1):
+            if(redundent_data[i+j*original_data_len] == True):
+                num_ones += 1
+            else:
+                num_zeros += 1
+        if(num_ones>num_zeros):
+            collapsed_data.set(pos=i, value=True)
+        else:
+            collapsed_data.set(pos=i, value=False)
+    return collapsed_data
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     file_name, _ = QFileDialog.getOpenFileName(None)
-    file_bin= get_file_binary(file_name= file_name)
-    get_png_from_binary(file_bin, "myImage")
+    file_bin = get_file_binary(file_name= file_name)
+    print(len(file_bin))
+    redundent_data = make_binary_redundent(file_bin, 3)
+    print(len(file_bin))
+    print((len(redundent_data)))
+    print(file_bin[0:10].bin)
+    print(redundent_data[0:10].bin)
+    collapsed_data = vote_corect_data(redundent_data, 3)
+    print(collapsed_data[0:10].bin)
+    get_png_from_binary(collapsed_data, "myImage")
     
